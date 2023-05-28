f592f4bcdc7e59e222a41115a0330afea229004d
slayonce <katharinemg@gmail.com>
2023-05-28 18:02:08 -0400

    added run method in cli.js and created new cli instance in index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const cli = require('./lib/cli.js');
-const logoGenerator = require('./lib/LogoGenerator.js');
+// SECTION: Require necessary modules
+const CLI = require('./lib/cli');
+
+const cli = new CLI();
+
+cli.run();
+
+
